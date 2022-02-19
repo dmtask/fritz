63bfee9567b32a7546bab0b5473cadfb3c75419a
Daniel <daniel.mertgen@gmx.de>
2022-02-19 17:23:05 +0100

    buttons

--- fritz.py
+++ fritz.py
@@ -9,11 +9,15 @@ class Screen(GridLayout):
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
 
-        fc = FritzConnection(address='192.168.178.1')
+        #fc = FritzConnection(address='192.168.178.1')
 
-        self.rows = 2
-        self.add_widget(Label(text=fc.modelname))
-        self.add_widget(Label(text='Hallo Welt...'))
+        layout = BoxLayout(orientation='horizontal')
+        btn1 = Button(text='Status')
+        btn2 = Button(text='WLAN an/aus')
+
+        layout.add_widget(Label(text='Fritzbox Panel'))
+        layout.add_widget(btn1)
+        layout.add_widget(btn2)
 
 class Fritz(App):
     def build(self):
