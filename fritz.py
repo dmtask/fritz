eddb6f2eb14c1552a9c69063e3e7e960977f5aa7
Daniel Mertgen <dm@eoceo.de>
2022-04-01 11:51:54 +0200

    .env File hinzugefügt, Button Klick reagiert nun, Codeverbesserungen

--- fritz.py
+++ fritz.py
@@ -6,15 +6,20 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from functools import partial
+import os
+from dotenv import load_dotenv
 
-class Screen():
-    def generateBoxLayout(self):
-        #fc = FritzConnection(address='192.168.178.1')
+
+class Screen:
+    def generate_box_layout(self):
+        # fc = FritzConnection(address=os.getenv('fritzbox_address'))
 
         layout = BoxLayout(orientation='vertical')
         btn1 = Button(text='Status')
-        btn1.bind(on_press=self.pressBtn1())
+        btn1.bind(on_press=partial(self.press_button, 'state'))
         btn2 = Button(text='WLAN an/aus')
+        btn2.bind(on_press=partial(self.press_button, 'wlan'))
 
         layout.add_widget(Label(text='Fritzbox Panel', color=(0, 0, 0)))
         layout.add_widget(btn1)
@@ -22,16 +27,33 @@ class Screen():
 
         return layout
 
-    def pressBtn1(self):
-        print('Btn1')
+    def press_button(self, value, instance):
+        print(instance)
 
-        return ''
+        if value == 'state':
+            self.__get_state()
+        elif value == 'wlan':
+            self.__wlan()
+        else:
+            print('Nicht unterstütze Eigenschaft!')
+
+    def __get_state(self):
+        print('state')
+
+    def __wlan(self):
+        print('wlan')
+
 
 class Fritz(App):
     def build(self):
+        # Load .env File
+        load_dotenv()
+
+        # Das Fenster soll einen weißen Hintergrund haben
         Window.clearcolor = (1, 1, 1, 1)
 
-        return Screen().generateBoxLayout()
+        return Screen().generate_box_layout()
+
 
 if __name__ == '__main__':
     Fritz().run()
